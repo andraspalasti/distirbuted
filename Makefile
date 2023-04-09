7a0ba62f63e3c14a930735191506f80e88e88373
andraspalasti <andraspalasti2@gmail.com>
2023-04-09 10:06:06 +0200

    completed broadcast b

--- Makefile
+++ Makefile
@@ -26,4 +26,4 @@ test-uniqueids: maelstrom build
 
 test-broadcast: maelstrom build
 	cd maelstrom/; \
-	./maelstrom test -w broadcast --bin ./broadcast --node-count 1 --time-limit 20 --rate 10
+	./maelstrom test -w broadcast --bin ./broadcast --node-count 5 --time-limit 20 --rate 10
--- broadcast/main.go
+++ broadcast/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"sync"
+	"time"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
@@ -18,12 +19,46 @@ type ReadMessageBody struct {
 	Messages []int `json:"messages"`
 }
 
+type WriteMessageBody struct {
+	maelstrom.MessageBody
+	Messages []int `json:"messages"`
+}
+
+type MsgStore[T any] struct {
+	sync.Mutex
+	messages []T
+}
+
+func NewMsgStore[T any]() *MsgStore[T] {
+	return &MsgStore[T]{
+		Mutex:    sync.Mutex{},
+		messages: []T{},
+	}
+}
+
+func (ms *MsgStore[T]) Read() []T {
+	ms.Lock()
+	defer ms.Unlock()
+
+	messages := make([]T, len(ms.messages))
+	copy(messages, ms.messages)
+	return messages
+}
+
+func (ms *MsgStore[T]) Put(messages ...T) {
+	ms.Lock()
+	defer ms.Unlock()
+
+	ms.messages = append(ms.messages, messages...)
+}
+
 func main() {
 	n := maelstrom.NewNode()
 
-	// We store the recieved messages here
-	messages := []int{}
-	mu := &sync.Mutex{}
+	// We store all the messages here
+	messages := NewMsgStore[int]()
+
+	unbroadcasted := NewMsgStore[int]()
 
 	n.Handle("broadcast", func(msg maelstrom.Message) error {
 		var body BroadcastMessageBody
@@ -31,26 +66,53 @@ func main() {
 			return err
 		}
 
-		mu.Lock()
-		messages = append(messages, body.Message)
-		mu.Unlock()
+		unbroadcasted.Put(body.Message)
 
 		return n.Reply(msg, maelstrom.MessageBody{Type: "broadcast_ok"})
 	})
 
 	n.Handle("read", func(msg maelstrom.Message) error {
-		mu.Lock()
-		defer mu.Unlock()
-
 		return n.Reply(msg, ReadMessageBody{
 			MessageBody: maelstrom.MessageBody{Type: "read_ok"},
-			Messages:    messages,
+			Messages:    messages.Read(),
 		})
+	})
+
+	n.Handle("write", func(msg maelstrom.Message) error {
+		var body WriteMessageBody
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		messages.Put(body.Messages...)
+		return nil
 	})
 
 	n.Handle("topology", func(msg maelstrom.Message) error {
 		return n.Reply(msg, maelstrom.MessageBody{Type: "topology_ok"})
 	})
+
+	// From time to time broadcast the unbroadcasted messages to each node
+	go func() {
+		ticker := time.NewTicker(time.Duration(200) * time.Millisecond)
+		for {
+			shouldWrite := unbroadcasted.Read()
+
+			for _, nodeId := range n.NodeIDs() {
+				if n.ID() == nodeId {
+					continue
+				}
+
+				n.Send(nodeId, WriteMessageBody{
+					MessageBody: maelstrom.MessageBody{Type: "write"},
+					Messages:    shouldWrite,
+				})
+			}
+
+			messages.Put(shouldWrite...)
+			<-ticker.C
+		}
+	}()
 
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
